93c539368aa1bc272afbe0c75919cc1a8529d498
AndreCaue <AndreCaue>
2023-08-29 19:00:14 -0300

    Changeing phrases 2.0

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const body = document.querySelector("body");
 const showMessage = document.querySelector(".underText");
 
 const openCookieByClick = () => {
+  if (imageCookie.classList.contains("openedCookie")) return;
   //image / change pic
-  buttonAnotherCookie.classList.toggle("hidden");
-  imageCookie.classList.toggle("openedCookie");
+  buttonAnotherCookie.classList.remove("hidden");
+  imageCookie.classList.add("openedCookie");
 
   // when you open a cookie = open mode
   body.classList.toggle("openMode");
@@ -18,6 +19,12 @@ const openCookieByClick = () => {
   }, 2000);
   setTimeout(() => {
     showMessage.innerText =
-      "A Arte de ser louco é jamais cometer a loucura de ser um sujeito normal ";
+      "A arte de ser louco é jamais cometer a loucura de ser um sujeito normal";
   }, 4000);
 };
+
+buttonAnotherCookie.addEventListener("click", (event) => {
+  imageCookie.classList.remove("openedCookie");
+  showMessage.innerText = "";
+  buttonAnotherCookie.classList.add("hidden");
+});
