626888e23eed74763a3e238b8a18e164a076e617
AndreCaue <AndreCaue>
2024-02-09 13:10:30 -0300

    just one phrase

--- index.js
+++ index.js
@@ -5,32 +5,7 @@ const showMessage = document.querySelector(".underText");
 
 const openCookieByClick = () => {
   const phraseArray = [
-    "A persistência realiza o impossível.",
-    "Não se desespere quando a caminhada estiver difícil, é sinal de que você está no caminho certo.",
-    "No meio da dificuldade encontra-se a oportunidade.",
-    "Imagine uma nova história para sua vida e acredite nela.",
-    "Pequenos gestos podem ter grandes impactos.",
-    "O verdadeiro conhecimento é saber que você não sabe tudo.",
-    "O passado só define o seu futuro se você permitir. ",
-    "A mente é como um paraquedas, só funciona se estiver aberta.",
-    "Seja a versão da qual você vai se orgulhar no futuro. ",
-    "as coisas que nos arrependeremos serão as decisões que não tomamos.",
-    "Deixe pra trás o que não te leva pra frente.",
-    "Os obstáculos são apenas oportunidades disfarçadas para deixar você mais forte.",
-    "Faça valer a pena, as oportunidades não voltam.",
-    "Tentar acertar realizando os mesmos erros é o mais claro sinal de insanidade.",
-    "As palavras podem mentir, mas as atitudes sempre falam a verdade.",
-    "A motivação é o que ajuda a começar. O hábito é o que faz seguir adiante.",
-    "O seu maior professor é o seu maior erro.",
     "Se você realmente quer que aconteça, vá atrás e não desista.",
-    "Tudo vem com um propósito, vai por uma razão e fica o essencial.",
-    "Decisões são assim, ou você arrisca, ou você se conforma.",
-    "O homem superior atribui a culpa a si próprio; o homem comum aos outros",
-    "Quando se está no fundo poço, voce aprende a olhar para cima",
-    "Lute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance. Invada. Caia. Seja tudo o quiser ser, mas, acima de tudo, seja você sempre.",
-    "Não é a carga que o derruba, mas a maneira como você a carrega",
-    "Não há caminho para felicidade, a felicidade é o caminho.",
-    "Uma ilusão pode parece bonita e agradável, mas lembre-se é apenas uma ilusão.",
   ];
 
   if (imageCookie.classList.contains("openedCookie")) return;
@@ -42,7 +17,7 @@ const openCookieByClick = () => {
 
   // h3
 
-  const numeroAle = Math.round(Math.random() * 26);
+  const numeroAle = Math.round(Math.random() * 1);
 
   phraseArray.map((item, index) => {
     console.log(phraseArray.length);
